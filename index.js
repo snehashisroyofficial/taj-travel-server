dda9d2cff47bfdc7f64ffefebdd73b7939b28b9e
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-04-28 00:04:28 +0530

    country based data successfully fetch

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/alltouristsspot/:id", async (req, res) => {
+    app.get("/viewdetails/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await touristsSpotCollection.findOne(query);
