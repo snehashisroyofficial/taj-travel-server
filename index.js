7c6d7d8ea1e839e8091128473bae7198dbaedf5a
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-04-27 15:38:35 +0530

    tourists spot data successfully fetch from mongodb

--- index.js
+++ index.js
@@ -52,6 +52,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/alltouristsspot", async (req, res) => {
+      const cursor = touristsSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
