737ca5cb6c14f871f3b7de715341747c2a89a3c0
Snehashis Roy <116051236+snehashisroyofficial@users.noreply.github.com>
2024-04-30 10:20:49 +0530

    vercel error found

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const countriesCollection = client
       .db("CountriesDB")
@@ -121,7 +121,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -131,6 +131,10 @@ async function run() {
   }
 }
 run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("coffee maker server is running");
+});
 
 app.listen(port, () => {
   console.log(`This port is running on ${port}`);
